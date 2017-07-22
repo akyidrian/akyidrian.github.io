6cab19372bb6134a3eafac8e92dfd1b41eedec08
akyidrian <akyidrian@gmail.com>
2017-07-22 14:36:46 +1200

    Changed .ico. Improved email.js. Fixed up CSS.

--- assets/js/email.js
+++ assets/js/email.js
@@ -2,9 +2,14 @@ document.addEventListener("DOMContentLoaded", function() {
     var userName = "akyidrian";
     var hostName = "gmail.com";
     var email = userName + "@" + hostName;
-    var emailText = document.getElementsByClassName("emailtext")[0];
-    var emailLink = document.getElementsByClassName("emaillink")[0];
+    
+    var emailLinkElems = document.querySelectorAll("a.email");
+    for (i = 0; i < emailLinkElems.length; i++) {
+    	emailLinkElems[i].href = "mailto:" +  email;
+    }
 
-    emailText.innerHTML = "<i class=\"fa fa-envelope-o\"></i>&nbsp; " + email;
-    emailLink.href = "mailto: " +  email;
+    var emailTextElems = document.querySelectorAll("span.email");
+    for (i = 0; i < emailTextElems.length; i++) {
+    	emailTextElems[i].innerHTML = email;	
+    }
 });
