05691d780b36a4f5434c43450f21d9656674da81
Akyidrian <akyidrian@gmail.com>
2018-03-31 01:00:25 -0700

    Updating fontawesome and adding blog icon

--- assets/js/email.js
+++ assets/js/email.js
@@ -3,13 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var hostName = "gmail.com";
     var email = userName + "@" + hostName;
     
-    var emailLinkElems = document.querySelectorAll("a.email");
-    for (i = 0; i < emailLinkElems.length; i++) {
-    	emailLinkElems[i].href = "mailto:" +  email;
-    }
-
-    var emailTextElems = document.querySelectorAll("span.email");
-    for (i = 0; i < emailTextElems.length; i++) {
-    	emailTextElems[i].innerHTML = email;	
-    }
+    var emailLinkElem = document.getElementById("email");
+  	emailLinkElem.href = "mailto:" +  email;
+    emailLinkElem.innerHTML += email;
 });
